33acaf6f05876743416c344e50f6bee5e67e32da
Katie <50029859+katieraby@users.noreply.github.com>
2020-05-28 14:40:24 +0100

    Remove Artist documents from local DB after the tests have executed

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,9 +1,18 @@
-const { createArtist } = require("../models/artistModel");
+const { createArtist, getArtistByUsername } = require("../models/artistModel");
 
 exports.postArtist = (req, res, next) => {
   return createArtist(req.body)
     .then((postedArtist) => {
       res.status(201).send(postedArtist);
+    })
+    .catch(console.dir);
+};
+
+exports.fetchArtistByUsername = (req, res, next) => {
+  const { username } = req.params;
+  return getArtistByUsername(username)
+    .then((artist) => {
+      res.status(200).send(artist);
     })
     .catch(console.dir);
 };
